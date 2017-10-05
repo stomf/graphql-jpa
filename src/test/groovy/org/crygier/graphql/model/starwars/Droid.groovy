347e5a72aa1dae747b9caf8bc9d88a4d0554ec27
tfraser <tfraser@datagraphic.co.uk>
2017-10-05 14:42:45 +0100

    Revert "logging"
    This reverts commit 19fff15834393166c72a78eae342ed50b73653a9.

--- src/test/groovy/org/crygier/graphql/model/starwars/Droid.groovy
+++ src/test/groovy/org/crygier/graphql/model/starwars/Droid.groovy
@@ -5,6 +5,7 @@ import org.crygier.graphql.annotation.GraphQLIgnore
 import org.crygier.graphql.annotation.SchemaDocumentation
 
 import javax.persistence.Entity
+import javax.persistence.OneToMany
 
 @Entity
 @SchemaDocumentation("Represents an electromechanical robot in the Star Wars Universe")
@@ -16,14 +17,10 @@ class Droid extends Character {
 
     // this causes an exception in GraphQLSchemaBuilder.getAttributeType
     // byte array not used very much in graphql.
-    // That's fine, because graphql isn't designed to send large chunks of data.
+    // That's fine, becuase graphql isn't designed to send large chunks of data.
     // It should probably instead post a url where the file can be obtained.
     // But the @GraphQLIgnore annotation should allow byte arrays to be defined in the entities
     // without throwing an exception.
-
-    // the @GraphQLIgnore annotation does work unless there's an object pointing to this one (Human has favourite droid)
-    // Also works if it is placed on the class (but might not want to ignore the whole class)
-
     @GraphQLIgnore
     byte[] data;
 
